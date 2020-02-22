f7fc11e3cb59d2e817573dac7d13f7d4e6cb2bfe
tpluscode <tpluscode@users.noreply.github.com>
2020-02-22 14:39:08 +0100

    test: that request headers can be changed

--- src/authentication.ts
+++
@@ -1,3 +0,0 @@
-export default (user: string, password: string) => {
-  return `Basic ${Buffer.from(`${user}:${password}`).toString('base64')}`
-}
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,6 @@
-import SparqlHttp from 'sparql-http-client'
-
-export interface SparqlBuilderInit {
-  baseUri?: string
-  username?: string
-  password?: string
-}
-
-const settings: SparqlBuilderInit = {}
-
-export default settings
-
-export interface SparqlQueryBuilder<TResult> {
-  build(): string
-  execute(client: SparqlHttp, requestInit?: RequestInit): Promise<TResult>
-}
-
-export { DELETE } from './DeleteBuilder'
-export { INSERT } from './InsertBuilder'
-export { ASK } from './AskBuilder'
-export { DESCRIBE } from './DescribeBuilder'
-export { CONSTRUCT } from './ConstructBuilder'
-export { SELECT } from './SelectBuilder'
+export { DELETE } from './lib/DeleteBuilder'
+export { INSERT } from './lib/InsertBuilder'
+export { ASK } from './lib/AskBuilder'
+export { DESCRIBE } from './lib/DescribeBuilder'
+export { CONSTRUCT } from './lib/ConstructBuilder'
+export { SELECT } from './lib/SelectBuilder'
