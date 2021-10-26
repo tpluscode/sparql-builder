ec53815bfe4e59355c6ab388a0bf3f57ee724e4a
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-10-26 12:47:36 +0200

    feat: export useful types

---
+++ .changeset/nasty-deers-arrive.md
@@ -0,0 +1,5 @@
+---
+"@tpluscode/sparql-builder": patch
+---
+
+Export types representing queries which can be executed and built
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import type * as Lib from './lib'
+
 export { sparql } from '@tpluscode/rdf-string'
 export { DELETE } from './lib/DeleteBuilder'
 export { INSERT } from './lib/InsertBuilder'
@@ -7,3 +9,8 @@ export { CONSTRUCT } from './lib/ConstructBuilder'
 export { SELECT } from './lib/SelectBuilder'
 export { WITH } from './lib/WithBuilder'
 export type { SparqlTemplateResult } from '@tpluscode/rdf-string'
+
+export type Select = Lib.SparqlQueryExecutable & Lib.SparqlQuery
+export type Construct = Lib.SparqlGraphQueryExecutable & Lib.SparqlQuery
+export type Update = Lib.SparqlUpdateExecutable & Lib.SparqlQuery
+export type Ask = Lib.SparqlAskExecutable & Lib.SparqlQuery
