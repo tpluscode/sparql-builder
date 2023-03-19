af6bb7a8443ce6f0d58c4393bf3d08e4480e12ec
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-03-19 15:16:40 +0100

    test: missing case

--- test/WITH.test.ts
+++ test/WITH.test.ts
@@ -1,4 +1,5 @@
 import { expect } from 'chai'
+import RDF from 'rdf-ext'
 import { DELETE, INSERT, WITH } from '../src/index.js'
 import './sparql.js'
 
@@ -13,12 +14,13 @@ describe('WITH', () => {
     // then
     expect(actual).to.be.query(expected)
   })
-  it('prepends WITH clause given as string', () => {
+
+  it('prepends WITH clause given as named node', () => {
     // given
     const expected = 'WITH <http://test.graph/> INSERT { ?s ?p ?o } WHERE { ?s ?p ?o }'
 
     // when
-    const actual = WITH('http://test.graph/', INSERT`?s ?p ?o`.WHERE`?s ?p ?o`).build()
+    const actual = WITH(RDF.namedNode('http://test.graph/'), INSERT`?s ?p ?o`.WHERE`?s ?p ?o`).build()
 
     // then
     expect(actual).to.be.query(expected)
