f48e216547985ce29051b463162624f4d83c6e17
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-08-23 11:43:51 +0200

    build(deps): anylogger

--- test/mochaSetup.cjs
+++
@@ -1,8 +0,0 @@
-var chai = require("chai");
-var sinonChai = require("sinon-chai");
-var chaiAsPromised = require("chai-as-promised");
-require('chai-snapshot-matcher');
-require('anylogger-debug')
-
-chai.use(chaiAsPromised);
-chai.use(sinonChai);
---
+++ test/mochaSetup.js
@@ -0,0 +1,7 @@
+import chai from 'chai'
+import sinonChai from 'sinon-chai'
+import chaiAsPromised from 'chai-as-promised'
+import 'anylogger-debug'
+
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
